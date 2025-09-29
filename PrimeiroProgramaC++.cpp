a25732965b14af8a0b0b09ead6b3cdb1655dccba
AfonsoES <afonsosespiritosanto@gmail.com>
2025-09-29 10:27:54 +0100

    Add files via upload

---
+++ PrimeiroProgramaC++.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"kguvwqygiasdyguiwafgyifahguasfjhafhjkfsahjkgfashasfjhfsahjfashjasfhasfhjgasfjhgasfghjasfjk\n";
+    std::cout<<"dasygasfghjyfashjgasfhjkasfjhksfahjgasfhjgasfhjasfashjgfsahjgasfjjasfjassjsafjkfhjashjasffhj"
+
+    return 0;
+}
