27dbb40ac6c4b467a9bdc7e1f7abacb12ed397ee
AfonsoES <afonsosespiritosanto@gmail.com>
2025-10-21 10:30:13 +0100

    Add files via upload

---
+++ Arrays/adivinha.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	srand(time(0));
+	int N = rand() % 100;
+	int r=-1, tentativas=0;
+
+	while (r != N)
+	{
+		cout << "Diz um número: ";
+		cin >> r;
+
+		if (r > N)
+		{
+			cout << "É menor.\n";
+			tentativas++;
+		}
+		else if (r < N)
+		{
+			cout << "É maior.\n";
+			tentativas++;
+		}
+		else
+		{
+			cout << "\nMuito bem, acertaste em" << tentativas;
+		}
+	}
+
+
+	return 0;
+}
