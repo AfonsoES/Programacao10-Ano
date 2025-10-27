2da8e6852e2c52fd80bb891c7040f63fcb8e6474
AfonsoES <afonsosespiritosanto@gmail.com>
2025-10-27 09:31:14 +0000

    Add files via upload

---
+++ Modulo2/Ordenar.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int N[10] = {15,7,54,12,1,2,5,23,14,4,}, Troca, Aux;
+
+	do
+	{
+		Troca = 0;
+
+		for (int i = 0; i < 9; i++)
+		{
+			if (N[i] > N[i + 1])
+			{
+				Troca++;
+				Aux = N[i];
+				N[i] = N[i + 1];
+				N[i + 1] = Aux;
+			}
+		}
+
+	} while (Troca != 0);
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << N[i] << "\n";
+	}
+}
